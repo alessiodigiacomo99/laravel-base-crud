cade701f360d92f4ba4a5f980e636aa8b7f8673a
alessio <alessiodg99.adg@gmail.com>
2021-02-18 17:00:22 +0100

    migrazione eseguita e creato routes e views

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!-- Istruzioni:
+Proseguendo con l'idea del blog create una crud base utilizzando la Route::resource() ed il resource controller implementando i metodi, index e create con annesse views.
+Cancellate la tabella posts dal database ed utilizzate il comando php artisan make:migration per creare una migation per la tabella posts. Poi popolatela manualmente via phpmyadmin. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
